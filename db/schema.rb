ba72da5157a874ff6989e502feb55d0c36482da2
Nykolas Lima <nykolas.lima@gmail.com>
2013-04-26 22:08:43 -0300

    adding Cliente model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130426221336) do
+ActiveRecord::Schema.define(:version => 20130427010636) do
+
+  create_table "clientes", :force => true do |t|
+    t.string   "nome",       :limit => 80
+    t.integer  "idade"
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
+  end
 
   create_table "restaurantes", :force => true do |t|
     t.string   "nome",          :limit => 80
