23ce350b1f423112ce514bd523ab528f2dd76f9d
Nykolas Lima <nykolas.lima@gmail.com>
2013-04-26 22:16:52 -0300

    adding some validation to Prato

--- app/models/prato.rb
+++ app/models/prato.rb
@@ -1,3 +1,7 @@
 class Prato < ActiveRecord::Base
 	attr_accessible :nome
+
+	validates_presence_of :nome, message: " - deve ser preenchido"
+	
+	validates_uniqueness_of :nome, message: " - nome já cadastrado"
 end
