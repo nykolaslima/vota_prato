010526f867c6474388da52cc8a8033298ca8eda4
Nykolas Lima <nykolas.lima@gmail.com>
2013-04-29 22:22:27 -0300

    adding show and destroy to Restaurante

--- app/controllers/restaurantes_controller.rb
+++ app/controllers/restaurantes_controller.rb
@@ -2,4 +2,14 @@ class RestaurantesController < ApplicationController
 	def index
 		@restaurantes = Restaurante.order :nome
 	end
+
+	def show
+		@restaurante = Restaurante.find(params[:id])
+	end
+
+	def destroy
+		Restaurante.destroy(params[:id])
+
+		redirect_to action: :index
+	end
 end
