c64d7ddd8f8bb9e8cc90e27fc5fad41631f32c9b
Nykolas Lima <nykolas.lima@gmail.com>
2013-05-02 19:58:38 -0300

    adding comments to Restaurante

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def valor_formatado(valor)
 		number_to_currency valor, unit: 'R$', separator: ',', delimiter: '.'
 	end
+
+	def novo_comentario(comentavel)
+		render partial: 'comentarios/novo_comentario', locals: {comentavel: comentavel}
+	end
 end
